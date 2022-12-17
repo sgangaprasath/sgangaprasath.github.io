fd290ce028933014b88ed280c4323af75022cd9b
S Ganga Prasath <s.gangaprasath1@gmail.com>
2022-12-17 16:00:38 -0500

    pub-working

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
         serif: ['Inter', 'serif'],
         mono: ['Fira Code', 'monospace'],
       },
+      colors: {
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
+        veryDarkBlue: 'hsl(229, 31%, 21%)',
+      },
     },
   },
   plugins: [],
