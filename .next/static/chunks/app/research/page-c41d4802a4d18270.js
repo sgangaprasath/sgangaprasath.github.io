a16f8ac4cf2e2039eee2cf2aaac5e1b7e82b8895
S Ganga Prasath <s.gangaprasath1@gmail.com>
2025-02-03 09:41:05 +0530

    OldDelete

--- .next/static/chunks/app/research/page-c41d4802a4d18270.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[214,607,281,834],{2087:function(e,n,s){Promise.resolve().then(s.t.bind(s,7477,23)),Promise.resolve().then(s.t.bind(s,5962,23))}},function(e){e.O(0,[477,962,253,769,744],function(){return e(e.s=2087)}),_N_E=e.O()}]);
